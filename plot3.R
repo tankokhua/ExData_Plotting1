d2ee513fda71012cf8df37626c070412a3e108c1
tankokhua <tankokhua@outlook.com>
2014-05-08 23:43:33 +0800

    - add plot3.*

---
+++ plot3.R
@@ -0,0 +1,16 @@
+dt <- read.table("household_power_consumption.txt",
+                  skip = 66637, nrow = 2880, sep = ";", 
+                  col.names = colnames(read.table(
+                    "household_power_consumption.txt",
+                    nrow = 1, header = TRUE, sep=";")))
+dt$Date <- as.Date(dt$Date, "%d/%m/%Y")
+dt$Date_Time <- paste(dt$Date, dt$Time)
+dt$Date_Time <- as.POSIXct(strptime(dt$Date_Time, "%Y-%m-%d %H:%M:%S"))
+plot(dt$Date_Time, dt$Sub_metering_1, xlab="", ylab="Global Active Power (kilowatts)", type="n")
+points(dt$Date_Time, dt$Sub_metering_1, type="l")
+points(dt$Date_Time, dt$Sub_metering_2, type="l", col="red")
+points(dt$Date_Time, dt$Sub_metering_3, type="l", col="blue")
+legend("topright", cex=0.7, lty=1, col=c("black", "red", "blue"), legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+dev.copy(png, file="plot3.png", height=480, width=480)
+dev.off()
