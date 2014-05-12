f7f2ef61ca44f5d07ef98cbbcbb7bc0296ad08e8
tankokhua <tankokhua@outlook.com>
2014-05-12 22:47:39 +0800

    - update.

--- plot1.R
+++ plot1.R
@@ -1,4 +1,8 @@
-dt <- read.table("a.txt", sep=";", header=TRUE)
+dt <- read.table("household_power_consumption.txt",
+                  skip = 66637, nrow = 2880, sep = ";", 
+                  col.names = colnames(read.table(
+                    "household_power_consumption.txt",
+                    nrow = 1, header = TRUE, sep=";")))
 par(mar=c(4,4,2,2))
 hist(dt$Global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts)", col="red")
 dev.copy(png, file="plot1.png", height=480, width=480)
